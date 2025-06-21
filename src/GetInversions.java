5cb4fb5bf243033ba18bd06934b9143a2608f1db
spathak072 <saurabhpathak072@gmail.com>
2025-06-21 23:43:13 +0530

    fix the typo.

--- src/GetInversions.java
+++ src/GetInversions.java
@@ -1,5 +1,8 @@
 //Problem statement
-//        You have a list of numbers that are not in order. You want to find the sum of the differences between the indices of pairs of numbers that are in the wrong order. A pair of numbers is considered to be in the wrong order if the smaller number comes after the larger number in the list.
+//        You have a list of numbers that are not in order.
+//        You want to find the sum of the differences between the indices of pairs of numbers
+//        that are in the wrong order. A pair of numbers is considered to be in the wrong order
+//        if the smaller number comes after the larger number in the list.
 //
 //        Example:
 //        Input: ‘N’ = 5
@@ -13,11 +16,11 @@
 //        6
 //        Explanation
 //        When we apply selection sort, first swap will happen between array[0] to array[4],
-//        inversion will be (index of arrar[4]-index of array[0]) = 4-0=4 and
+//        inversion will be (index of array[4]-index of array[0]) = 4-0=4 and
 //        the array after this will be [1, 2, 11, 5, 3], still the array is not sorted.
 //        So we will apply selection sort again,
 //        the swap will now take place between array[2] and array[4],
-//        inversion will be 4+(index of arrar[4]-index of array[2])=4+(4-2)=4+2=6.
+//        inversion will be 4+(index of array[4]-index of array[2])=4+(4-2)=4+2=6.
 //        So the final sorted array now will be [1,2,3,5,11]
 public class GetInversions {
     public static int getInversions(int[] arr, int n){
